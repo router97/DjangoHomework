e3be5691719789633e4bc331a209e8a07089d354
router <you@example.com>
2024-03-13 17:08:29 +0200

    added edit

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('article/<int:id>/', views.article, name='article'),
     path('add-article/', views.add_article, name='add-article'),
     path('delete-article/<int:id>/', views.delete_article, name='delete-article'),
+    path('edit-article/<int:id>/', views.edit_article, name='edit-article'),
     path('like/<int:article_id>/', views.like, name='like'),
     path('like-comment/<int:comment_id>/', views.like_comment, name='like-comment'),
     path('dislike/<int:article_id>/', views.dislike, name='dislike'),
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from .models import Article, Comment
@@ -9,7 +9,7 @@ def article(request: HttpRequest, id: int) -> HttpResponse:
     article = get_object_or_404(Article, id=id)
     article.views += 1
     article.save()
-    form_comment = CommentForm
+    form_comment = CommentForm()
     context = {
         'article': article,
         'form_comment': form_comment
@@ -17,63 +17,104 @@ def article(request: HttpRequest, id: int) -> HttpResponse:
     return render(request, 'article.html', context)
 
 @login_required
-def add_article(request: HttpRequest):
-    if request.method == 'POST':
-        form = ArticleForm(request.POST, request.FILES)
-        if form.is_valid():
-            article = form.save(commit = False)
-            article.author = request.user
-            article.save()
-            messages.success(request, 'Created article')
-            return redirect('blog:article', id = article.id)  
-    else:
-        form = ArticleForm()  
-    context = {'form': form}
-    return render(request, 'article_form.html', context)
+def add_article(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+    
+    if request.method != 'POST':
+        form = ArticleForm() 
+        context = {'form': form}
+        return render(request, 'article_form.html', context)
+    
+    form = ArticleForm(request.POST, request.FILES)
+    
+    if not form.is_valid():
+        messages.error(request, 'Failed to create article')
+        context = {'form': form}
+        return render(request, 'article_form.html', context)
+    
+    article = form.save(commit=False)
+    article.author = request.user
+    article.save()
+    messages.success(request, 'Created article')
+    return redirect('blog:article', id=article.id)  
 
 @login_required
 def delete_article(request: HttpRequest, id: int):
     article = get_object_or_404(Article, id=id)
+    
     if request.user != article.author:
         messages.error(request, 'You are not the author of this article')
     else:
         messages.success(request, f'Sucessfully deleted article "{article.title}"')
         article.delete()
+    
     next_url = request.META.get('HTTP_REFERER', '/')
     return redirect(next_url)
     
-        
+@login_required
+def edit_article(request: HttpRequest, id: int) -> HttpResponse | HttpResponseRedirect:
+    article = get_object_or_404(Article, id=id)
+    
+    if request.user != article.author:
+        messages.error(request, 'You are not the author of this article')
+        next_url = request.META.get('HTTP_REFERER', '/')
+        return redirect(next_url)
+    
+    if request.method != 'POST':
+        form = ArticleForm(instance=article)
+        context = {'form': form}
+        return render(request, 'article_form.html', context)
+
+    form = ArticleForm(request.POST, request.FILES, instance=article)
+    
+    if not form.is_valid():
+        context = {'form': form}
+        messages.error(request, 'Failed to edit the article')
+        return render(request, 'article_form.html', context)
+    
+    form.save()
+    messages.success(request, f'Edited article {article.id} successfully')
+    return redirect('blog:article', id = article.id)
 
 @login_required
 def add_comment(request: HttpRequest, article_id: int):
-    if request.method == 'POST':
+    if request.method != 'POST':
+        form = CommentForm
+    
+    else:
         article = get_object_or_404(Article, id=article_id)
         form = CommentForm(request.POST)
-        if form.is_valid():
+        
+        if not form.is_valid():
+            messages.error(request, 'Failed to add comment')
+            
+        else:
+            messages.success(request, 'Added comment sucessfully')
             comment = form.save(commit=False)
             comment.article = article
             comment.author = request.user
             comment.save()
-    else:
-        form = CommentForm() 
+    
     next_url = request.META.get('HTTP_REFERER', '/')
     return redirect(next_url)
 
-def like(request: HttpRequest, article_id: int):
+@login_required
+def like(request: HttpRequest, article_id: int) -> HttpResponseRedirect:
     article = get_object_or_404(Article, id=article_id)
     article.likes += 1
     article.save()
     next_url = request.META.get('HTTP_REFERER', '/')
     return redirect(next_url)
 
-def dislike(request: HttpRequest, article_id: int):
+@login_required
+def dislike(request: HttpRequest, article_id: int) -> HttpResponseRedirect:
     article = get_object_or_404(Article, id=article_id)
     article.dislikes += 1
     article.save()
     next_url = request.META.get('HTTP_REFERER', '/')
     return redirect(next_url)
 
-def like_comment(request: HttpRequest, comment_id: int):
+@login_required
+def like_comment(request: HttpRequest, comment_id: int) -> HttpResponseRedirect:
     comment = get_object_or_404(Comment, id=comment_id)
     comment.likes += 1
     comment.save()
